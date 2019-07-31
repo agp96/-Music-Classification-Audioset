c71b6af34d05008e00aa9e415e91ef746a117145
agp08 <agpalmoradi@hotmail.com>
2019-07-31 16:53:53 +0200

    Update processor.py

--- audio/processor.py
+++ audio/processor.py
@@ -148,7 +148,7 @@ class WavProcessor(object):
         total_mood = 0
         for j in range(top_mood[0], top_mood[len(top_mood)-1]):
           total_mood = total_mood + predictions[0][j]
-        for j in range(first_class, second_class):
+        for j in range(top_mood[0], top_mood[len(top_mood)-1]):
           predictions[0][j] = predictions[0][j] / total_mood
         #print(predictions[0][276])
         #print(predictions[0][277])
