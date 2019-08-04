525d2b7ec5bf4610c403f9874e568604062ed0f7
agp08 <agpalmoradi@hotmail.com>
2019-08-04 12:10:09 +0200

    Update parse_file.py

--- parse_file.py
+++ parse_file.py
@@ -38,7 +38,7 @@ def process_file(wav_file, class_labels, to_csv, output_file, ten_seconds, num_p
     if not files:
         raise IOError("Unable to find input files. data_pattern='" +wav_file + "'")
     print(len(files))
-    for i in range(0,500):
+    for i in range(0,300):
         print(str(i))
         print(files[i])
         if not files[i]:
