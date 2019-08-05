b494589eea0e9a9077e0c4b6b1bd507afcf89176
agp08 <agpalmoradi@hotmail.com>
2019-08-05 18:54:32 +0200

    Update processor.py

--- audio/processor.py
+++ audio/processor.py
@@ -134,28 +134,26 @@ class WavProcessor(object):
         top_indices = np.argpartition(predictions[0], -count)[-count:]
         labels  = class_labels.split(",")
         top_mood = np.zeros(shape=(len(labels)), dtype=int)
-        print(top_mood)
-        print(class_labels)
-        print(labels)
+		
         for k in range(0, len(labels)):
           top_mood[k] = labels[k]
           print(top_mood[k])
-        print(top_mood)
-        print(len(top_mood))
+        #print(top_mood)
+        #print(len(top_mood))
 		
         total_mood = 0
         end_mood = top_mood[len(top_mood)-1]+1
         for j in range(top_mood[0], end_mood):
           total_mood = total_mood + predictions[0][j]
-          print(j)
+          #print(j)
         
         for j in range(top_mood[0], end_mood):
           predictions[0][j] = predictions[0][j] / total_mood
-          print(j)
+          #print(j)
 		
         line = ((self._class_map[i], float(predictions[0][i])) for
                 i in top_mood if predictions[0][i] > hit)
-        print(line)
+        #print(line)
         return sorted(line, key=lambda p: -p[1])
 
     def _process_features(self, features):
